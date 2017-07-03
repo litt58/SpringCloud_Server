5ed778a784457419fbad9e8fa4534cf002a28dbb
litt58 <lx520520>
2017-07-03 10:59:54 +0800

    修改注释

--- src/main/java/com/jzli/spring/cloud/demo/SpringCloudDemoApplication.java
+++ src/main/java/com/jzli/spring/cloud/demo/SpringCloudDemoApplication.java
@@ -4,16 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * =======================================================
- *
- * @Company 金色家网络科技有限公司-开发测试云服务部
- * @Date ：2016/8/4
- * @Author ：li.jinzhao
- * @Version ：0.0.1
- * @Description ：
- * ========================================================
- */
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudDemoApplication {
